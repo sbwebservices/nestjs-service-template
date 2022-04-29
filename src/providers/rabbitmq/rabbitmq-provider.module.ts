a0727857fd62a564c2e527994cb9722891d70a68
Douglas <douglas.hinckel@roundpe.gs>
2022-04-29 18:49:26 -0300

    fix: docker-compose up fix

--- src/providers/rabbitmq/rabbitmq-provider.module.ts
+++ src/providers/rabbitmq/rabbitmq-provider.module.ts
@@ -6,6 +6,7 @@ import {
   RabbitMQEnvConfig,
 } from '../../config/configuration';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import * as inspector from 'inspector';
 
 @Module({
   imports: [
@@ -14,16 +15,25 @@ import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService<AppConfig>) => {
         const config = configService.get<RabbitMQEnvConfig>('rabbitMQ');
-        return {
+
+        let endpoint = config.host;
+
+        if (process.env.NODE_ENV === 'local' && inspector.url()) {
+          endpoint = 'http://localhost:4566';
+        }
+
+        const params: RabbitMQConfig = {
           exchanges: [
             {
               name: RabbitMQConstants.exchange,
               type: RabbitMQConstants.exchange_type,
             },
           ],
-          uri: `amqp://${config.user}:${config.pass}@${config.host}:${config.port}`,
-          connectionInitOptions: { wait: false },
+          uri: `amqp://${config.user}:${config.pass}@${endpoint}:${config.port}`,
+          connectionInitOptions: { wait: true, timeout: 20000 },
         };
+
+        return Promise.resolve(params);
       },
     }),
   ],
